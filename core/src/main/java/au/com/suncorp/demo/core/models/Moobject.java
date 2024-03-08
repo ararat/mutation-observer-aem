cdfd980ee428d61ad520c5bebcc9d9884b85abb2
Yuval Ararat <ararat@adobe.com>
2024-03-08 17:06:37 +1300

    Adding changes to java files

--- core/src/main/java/au/com/suncorp/demo/core/models/Moobject.java
+++ core/src/main/java/au/com/suncorp/demo/core/models/Moobject.java
@@ -1,5 +1,9 @@
 package au.com.suncorp.demo.core.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -33,8 +37,28 @@ public class Moobject {
     @ValueMapValue
     private String icon;
 
-
+    @JsonIgnore
     public String getRandom() {
         return Integer.toString((int) (Math.random() * 1000));
+    }
+
+    //    public String getJSON() {
+//        return "{ \"title\": \"" + title + "\", \"desc\": \"" + desc + "\", \"type\": \"" + type + "\", \"offerTitle\": \"" + offerTitle + "\", \"offerDesc\": \"" + offerDesc + "\", \"icon\": \"" + icon + "\" }";
+//    }
+
+
+    @JsonIgnore
+    public String getJson() {
+        String jsonString = "no value";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.valueToTree(this);
+        try {
+            return mapper.writeValueAsString(node);
+        } catch (
+                JsonProcessingException e) {
+            log.error("Cannot output json", e);
+        }
+        return jsonString;
+
     }
 }
